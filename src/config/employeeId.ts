ffd8d9a01aafaf527c0b04bd7d73d1f8d7165b5f
AnuragLovesCode <anurag.neet01@gmail.com>
2025-08-22 12:30:48 +0530

    change genrate employeeId method

--- src/config/employeeId.ts
+++ src/config/employeeId.ts
@@ -1,16 +1,37 @@
-// import Counter from "../models/counter.js";
+// // import Counter from "../models/counter.js";
 
-import EmployeeIdModel from "../models/employeeId.model";
+import EmployeeModel from "../models/employee.model";
 
-async function getNextEmployeeId() {
-  const counter = await EmployeeIdModel.findOneAndUpdate(
-    { name: "employee" },        // fixed counter for employee
-    { $inc: { seq: 1 } },        // increase by 1
-    { new: true, upsert: true }  // create if not exist
-  );
+// import EmployeeIdModel from "../models/employeeId.model";
 
-  // format: shivemp + 3 digit padded number
-  return "shivemp" + counter.seq.toString().padStart(3, "0");
+// async function getNextEmployeeId() {
+//   const counter = await EmployeeIdModel.findOneAndUpdate(
+//     { name: "employee" },        // fixed counter for employee
+//     { $inc: { seq: 1 } },        // increase by 1
+//     { new: true, upsert: true }  // create if not exist
+//   );
+
+//   // format: shivemp + 3 digit padded number
+//   return "shivemp" + counter.seq.toString().padStart(3, "0");
+// }
+
+
+
+
+
+async function generateEmployeeId() {
+  const lastEmployee = await EmployeeModel.findOne().sort({ createdAt: -1 });
+
+  if (!lastEmployee || !lastEmployee.employeeId) {
+    return "shivemp001";
+  }
+
+  // extract number part
+  const lastNumber = parseInt(lastEmployee.employeeId.replace("shivemp", ""));
+  const newNumber = lastNumber + 1;
+
+  return "shivemp" + newNumber.toString().padStart(3, "0");
 }
 
-export default getNextEmployeeId;
+
+export default generateEmployeeId;
