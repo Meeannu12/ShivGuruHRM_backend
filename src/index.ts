bba5800404ef6de8e3fbe85c1b3ebceac28c91e7
AnuragLovesCode <anurag.neet01@gmail.com>
2025-08-31 20:31:09 +0530

    chnage origin url

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", credentials: true },
+  cors: { origin: "https://office.shivgurusoftware.com", credentials: true },
 });
 
 app.use(express.json()); // parse JSON body
