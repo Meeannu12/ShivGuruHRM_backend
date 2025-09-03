e39536c184f036450d7b5881dfa863612e275014
AnuragLovesCode <anurag.neet01@gmail.com>
2025-09-03 15:25:46 +0530

    add submit project api and change project status done

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import ProjectModel, { IProject } from "../models/project.model";
+import { AuthRequest } from "../middleware/auth";
 
 export const createProject = async (req: Request, res: Response) => {
   try {
@@ -44,7 +45,9 @@ export const createProject = async (req: Request, res: Response) => {
 
 export const getAllProject = async (req: Request, res: Response) => {
   try {
-    const newProject = await ProjectModel.find().populate("client");
+    const newProject = await ProjectModel.find({ status: "pending" }).populate(
+      "client"
+    );
     const pendingProject = await ProjectModel.countDocuments({
       status: "pending",
     });
@@ -54,6 +57,33 @@ export const getAllProject = async (req: Request, res: Response) => {
       message: "Get All Project Successfully",
       project: newProject,
       pendingProject,
+    });
+  } catch (error: any) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const submitProject = async (req: AuthRequest, res: Response) => {
+  const user = req.user;
+  // console.log("user Response", user);
+
+  const { submitDate, id } = req.body;
+  try {
+    // console.log("submit Project Date", submitDate);
+
+    const newSubmitProject = await ProjectModel.findById(id);
+    if (!newSubmitProject)
+      return res
+        .status(404)
+        .json({ success: false, message: "Project not Exist In DB" });
+
+    newSubmitProject.submitDate = submitDate;
+    newSubmitProject.status = "complete";
+    await newSubmitProject.save();
+
+    res.status(201).json({
+      success: true,
+      message: `project Submit on ${new Date(submitDate).toISOString().split("T")[0]}`,
     });
   } catch (error: any) {
     res.status(500).json({ success: false, message: error.message });
--- src/routes/project.route.ts
+++ src/routes/project.route.ts
@@ -3,11 +3,13 @@ import { authMiddleware } from "../middleware/auth";
 import {
   createProject,
   getAllProject,
+  submitProject,
 } from "../controllers/project.controller";
 
 const projectRoute = Router();
 
 projectRoute.post("/createProject", authMiddleware, createProject);
 projectRoute.get("/getAllProject", authMiddleware, getAllProject);
+projectRoute.post("/submitProject", authMiddleware, submitProject)
 
 export default projectRoute;
