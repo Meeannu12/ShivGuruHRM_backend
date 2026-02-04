8850478a870f77dca1885550f336ef523cd24a2c
AnuragLovesCode <anurag.neet01@gmail.com>
2026-02-04 16:09:28 +0530

    change delete url

--- src/routes/trustBill.route.ts
+++ src/routes/trustBill.route.ts
@@ -7,6 +7,6 @@ const trustRoute = Router()
 
 trustRoute.post("/trust", upload.single("image"), createTrustBill)
 trustRoute.get("/trust", getTrustBill)
-trustRoute.delete("/truest/:id", trustDeleteById)
+trustRoute.delete("/trust/:id", trustDeleteById)
 
 export default trustRoute
